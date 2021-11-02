2e3c3076c75dff3f47393f00f7cf4e65de60c9b6
JoaoPedroPP <jp052@hotmail.com>
2021-11-02 19:10:48 -0300

    [development] added checkname for merge methods

--- src/services/github/mod.rs
+++ src/services/github/mod.rs
@@ -1,3 +1,5 @@
 mod github;
+mod check_names;
 
 pub use github::repo;
+pub use check_names::check_merge_methods;
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -5,3 +5,4 @@ mod github;
 pub use config::config_github;
 pub use data_structures::*;
 pub use github::repo;
+pub use github::check_merge_methods;
